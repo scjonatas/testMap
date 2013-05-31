31d23e9dcc677d3a57d034d9d09fceb314c722b7
scjonatas <scjonatas@gmail.com>
2013-05-31 17:01:29 -0300

    teste

--- js/index.js
+++ js/index.js
@@ -1,24 +1,4 @@
-$(document).ready(function() {
-    
-    alert("ae 2");
-    
-});
-
-
-alert("ae!");
-    
-    // In the callback you can use "this" to call a function (e.g. this.get('map') instead of $('#map_canvas').gmap('get', 'map'))
-    $('#map_canvas').gmap({
-        'callback' : function() {
-            var self = this;
-            // we can't use "this" inside the click function (that refers to the marker object in this example)
-            self.addMarker({
-                'position' : '57.7973333,12.0502107',
-                'bounds' : true
-            }).click(function() {
-                self.openInfoWindow({
-                    'content' : 'Hello World!'
-                }, this);
-            });
-        }
-    });
+function onDeviceReady(){
+    var map = new GoogleMap();
+    map.initialize();
+}
